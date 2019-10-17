e3d55ca0cddd2c85c2f7773f13d4393f9467eed7
alhaqhassan <alhaqhassan@gmail.com>
2019-10-17 16:59:47 +0530

    added some pages

--- src/constants/routes/main.paths.constant.js
+++ src/constants/routes/main.paths.constant.js
@@ -3,3 +3,7 @@ export const SIGN_IN_PATH = '/sign-in/';
 
 export const SEEKER_SIGNUP_PATH = '/seeker/sign-up/';
 export const EMPLOYER_SIGNUP_PATH = '/employer/sign-up/';
+export const EMPLOYER_HOME_PATH = '/employer/home/';
+export const SEEKER_HOME_PATH = '/seeker/home/';
+export const SEEKER_JOB_AVAILABLE_PATH = '/seeker/job-available/';
+export const SEEKER_JOB_RECRUITMENT_DETAILS_PATH = '/seeker/job-RECRUITMENT-DETAILS/';
